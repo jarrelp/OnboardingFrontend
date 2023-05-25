ff23fa58a5e1e1c8db2842ed537464c879cf4956
jarrelp <jarrel.pothoff@gmail.com>
2023-05-26 01:02:37 +0200

    small changes: taken scherm 112354

--- src/screens/home/TaskScreen.js
+++ src/screens/home/TaskScreen.js
@@ -1,17 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+import { SegmentedButtons, Text } from "react-native-paper";
 
-import CustomHeader from "../../components/CustomHeader";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+// import CustomHeader from "../../components/CustomHeader";
 
 const TaskScreen = () => {
+  const [value, setValue] = React.useState("");
+
   return (
-    <View>
-      <CustomHeader title={"Taken"} />
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Task Screen</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Icon name="compass-outline" size={300} />
+      <Text>Deze taak bevindt zich op: </Text>
+      <SegmentedButtons
+        value={value}
+        onValueChange={setValue}
+        buttons={[
+          {
+            value: "Verdieping1",
+            label: "Verdieping1",
+          },
+          {
+            value: "Verdieping2",
+            label: "Verdieping2",
+          },
+          { value: "Verdieping3", label: "Verdieping3" },
+        ]}
+      />
+      <View style={styles.taken}></View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  taken: {
+    backgroundColor: "#e17000",
+    height: "100%",
+    width: "100%",
+  },
+});
 
 export default TaskScreen;
